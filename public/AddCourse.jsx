99a96ff7596a7581d85f17911257637d3cc494d6
KajavathananM <47623225+KajavathananM@users.noreply.github.com>
2019-06-03 19:21:53 +0530

    Update AddCourse.jsx

--- public/AddCourse.jsx
+++ public/AddCourse.jsx
@@ -1,7 +1,59 @@
 import React, {Component} from 'react';
+import {axios} from  'axios';
 
 class AddCourse extends Component {
-    onChange(){
+
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            code:null,
+            name:null,
+            passMark:null,
+            lecturer:null,
+            subjects:null
+        };
+        this.onCodeChange=this.onCodeChange.bind(this);
+        this.onNameChange=this.onNameChange.bind(this);
+        this.onPassChange=this.onPassChange.bind(this);
+        this.onlecChange=this.onlecChange.bind(this);
+        this.onSubjectsChange=this.onSubjectsChange.bind(this);
+
+        this.onClick=this.onClick.bind(this);
+    }
+    onCodeChange(e){
+        this.setState({code:e.target.value});
+    }
+    onNameChange(e){
+        this.setState({name:e.target.value});
+    }
+    onPassChange(e){
+        this.setState({passMark:e.target.value});
+    }
+    onlecChange(e){
+        this.setState({ lecturer:e.target.value});
+    }
+    onSubjectsChange(e){
+        this.setState({ subjects:e.target.value});
+    }
+
+    onClick(){
+        console.log(this.state);
+        console.log(this.state.name);
+         var re = /""\s|""|\s/;
+         var subjects = this.state.subjects.split(re);
+         console.log( subjects );
+          axios.post('http://localhost:3000/course/',{
+              name:this.state.name,
+              code:this.state.code,
+              passMark:this.state.passMark,
+              lecturer:this.state.lecturer,
+              subjects:subjects.map(subject=>{
+                  return subject
+              })
+          }).then(
+              alert("Added new course")
+          );
 
     }
     render() {
@@ -14,7 +66,9 @@ class AddCourse extends Component {
                            <td><label><b>Name</b></label></td>
                            <td><input
                                        type="text"
-                                       name="name"/>
+                                       name="name"
+                                       onChange={this.onNameChange}
+                           />
                            </td>
                        </tr>
                        <br/>
@@ -22,15 +76,20 @@ class AddCourse extends Component {
                            <td><b>Code</b></td>
                            <td><input
                                       type="text"
-                                      name="code"/>
+                                      name="code"
+                                      onChange={this.onCodeChange}
+                           />
                            </td>
                        </tr>
                        <br/>
                        <tr>
                            <td><b>Pass Mark</b></td>
                            <td><input
-                                      type="text"
-                                      name="pass"/>
+                                      type="number"
+                                      name="pass"
+
+                                      onChange={this.onPassChange}
+                           />
                            </td>
                        </tr>
                        <br/>
@@ -38,20 +97,27 @@ class AddCourse extends Component {
                            <td><b>Lecturer in Charge</b></td>
                            <td><input
                                        type="text"
-                                       name="lecturer"/>
+                                       name="lecturer"
+                                       onChange={this.onlecChange}
+                           />
                            </td>
                        </tr>
                        <br/>
                        <tr>
                            <td><b>Subjects</b></td>
+                           <td><input
+                                  type="textArea"
+                                  name="subjects"
+                                  onChange={this.onSubjectsChange}
+                           /></td>
                        </tr>
                        <br/>
                        <tr>
-                           <td><div style={divStyle}><button style={btnStyle}>Create course</button></div></td>
+                           <td><div style={divStyle}><button style={btnStyle} onClick={this.onClick}>Create course</button></div></td>
                        </tr>
                     </tbody>
                 </table>
-
+                {console.log(this.state)}
             </div>
         );
     }
