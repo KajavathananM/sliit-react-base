1ea85baf4c92d3f7a5ba58cd402ecf100ff91099
KajavathananM <47623225+KajavathananM@users.noreply.github.com>
2019-06-04 17:14:27 +0530

    Update Courses.jsx

--- public/Courses.jsx
+++ public/Courses.jsx
@@ -1,39 +1,41 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link} from "react-router-dom";
-import AddCourse from "./AddCourse";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Courses extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        this.onClick=this.onClick.bind(this);
+
+        this.state = {
+            courses: []
+        };
     }
-    onClick(id){
-        console.log(id);
-        window.location.href='/courseInfo/'.concat(id);
+
+    componentDidMount() {
+        axios.get('/course')
+            .then((response) => {
+                this.setState({ courses: response.data.data })
+            });
     }
+
+
     render() {
         return (
             <div>
-                <h3 style={{textAlign:"center"}}>Available Courses</h3>
+                <h3 style={{ textAlign: "center" }}>Available Courses</h3>
                 <ul style={listStyle}>
-                    <li><button style={btnStyle} onClick={() => this.onClick("SE2018")}>Software Engineering</button></li>
-                    <li><button style={btnStyle} onClick={() => this.onClick("CN2018")}>Networking</button></li>
+                    {this.state.courses.map(course =>
+                        <li key={course._id}><a href={'/courseinfo/' + course.code}  >{course.name}</a></li>
+                    )
+                    }
                 </ul>
             </div>
         );
     }
 }
-const listStyle={
-    textAlign:"center",
-    listStyleType:"none"
-};
-const btnStyle={
-    width:"200px",
-    height:"50px",
-    color:"black",
-    background:"none",
-    border:"none"
+const listStyle = {
+    textAlign: "center",
+    fontSize:"20px",
+    listStyleType: "none"
 };
 
 export default Courses;
